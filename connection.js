433fb74a6bbb2fe15280ecd73e0c2c76d036e922
Alark Bhavsar <alarkb1297@gmail.com>
2017-11-28 20:19:16 -0500

    testing queries

--- connection.js
+++ connection.js
@@ -5,12 +5,18 @@ var app = express();
 var connection = mysql.createConnection({
     host: 'localhost',
     user: 'root',
-    password: 'Alark212'
+    password: 'Alark212',
+    database: 'starwarsFINAL'
 });
 
 connection.connect(function(err) {
     if (err) throw err
     console.log('You are now connected with mysql database...')
+});
+
+connection.query("select * from characters", function (err, result) {
+    if (err) throw err;
+    console.log(result);
 });
 
 module.exports = connection;
